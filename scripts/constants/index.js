440cdd3b4956628a4de9fdebe62dce53901a6673
jupiterBill <105947408+jupiterBill@users.noreply.github.com>
2022-11-14 20:48:58 +0100

    first commit

---
+++ scripts/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2c6f9E8CFA046A3Dd3876445fB9968EF30237060";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
